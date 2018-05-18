331c19231b7f208b846afaefae5619bf8c747c40
Simona <s.colapicchioni@gmail.com>
2018-05-18 16:03:03 +0200

    day5

---
+++ WebApplicationSecurity/WebApplicationSecurity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationSecurity.Startup))]
+namespace WebApplicationSecurity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
